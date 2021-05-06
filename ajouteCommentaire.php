57c845735dd8307bc166303ccd80e5e5addfa1bc
Arkaadon <tonygonthier974@gmail.com>
2021-05-06 15:02:28 +0200

    second commit

--- ajouteCommentaire.php
+++ ajouteCommentaire.php
@@ -2,7 +2,26 @@
 //Pour vérifier si $_POST n'est pas vide, utiliser !isset || Pour vérifier si c'est vide alors: isset($_POST)
     // file_put_contents('commentaire.JSON', json_encode($_POST));
 
-    $ressourceFile = fopen('commentaire.txt', 'a'); // récupération (et création si inexistant) du fichier json
-    fwrite($ressourceFile, json_encode($_POST).','); // écriture dans le fichier json en format string
-    fclose($ressourceFile); // fermeture du fichier json
-    header('Location: message.php');
+    // $ressourceFile = fopen('commentaire.txt', 'a'); // récupération (et création si inexistant) du fichier json
+    // fwrite($ressourceFile, json_encode($_POST).','); // écriture dans le fichier json en format string
+    // fclose($ressourceFile); // fermeture du fichier json
+    // header('Location: message.php');
+
+    // On crée un objet PDO, qui nous permettra de faire le lien entre le code fonctionnel de php et la BDD
+    // Le Dsn est un ensemble de paramètres séparés par des ';' qui représente le chemin d'accès à notre BDD, le username et le pwd ne sont rien
+    // d'autre que les identifiants de connection
+    
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=livre_dor;port=3306','root',''
+        ,[
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    } catch (PDOException $e) {
+        echo "Une erreur est survenue lors de la connexion à la base de donnée. Voici ce qu'elle retourne: ".$e->getMessage();
+        die();
+    }
+
+    // $pdo->exec("SELECT * FROM commentaires");
+    // $pdo->exec("INSERT INTO commentaires VALUES('Miguel', 'Kenneth ye vé fair oune burnoute')");
+    // $pdo->exec("INSERT INTO commentaires ('prenom') VALUES('Kenneth')");
--- message.php
+++ message.php
@@ -10,6 +10,8 @@
     <h1>Merci monsieur</h1>
     <h2>Les messages des autres monsieurs</h2>
     <?php
+    error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+    if (is_string(file_get_contents('commentaire.txt'))) {
         // on casse la chaîne de caractères à chaque occurence d'une autre chaine, explode nous retourne ainsi
         // un tableau plus exploitable
         $datas = explode('},', file_get_contents('commentaire.txt'));
@@ -20,9 +22,14 @@
                 // On décole le JSON afin de récupérer une donnée plus exploitable, ici un objet de type stdClass
                 $dataArray = json_decode($data);
                 // A l'aide des propriétés de l'objet dataArray on affiche le prénom et le message
-                echo $dataArray->Prenom.' a dit que: '.$dataArray->Message.'<br>';
-            }
-        }
+                echo filter_var($dataArray->Prenom, FILTER_SANITIZE_FULL_SPECIAL_CHARS).' a dit que: '.filter_var($dataArray->Message, FILTER_SANITIZE_FULL_SPECIAL_CHARS).'<br>';
+            };
+        };
+    }
+    else {
+        echo '<h3>Aucun commentaire n\'a été soumi</h3>';
+    };
+        
     ?>
 </body>
 </html>
